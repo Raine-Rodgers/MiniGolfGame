71a3ef2ebf7a3e831dd11f54c56a03a3390fe39f
Raine <80836177+Your-Average-Arsonist@users.noreply.github.com>
2025-01-05 17:41:41 +0100

    added map 2

---
+++ Physics Engine Template/Maps.cpp
@@ -0,0 +1,141 @@
+#include "Maps.h"
+
+
+Maps::Maps()
+{
+	_wall1 = new Rigid_Body(true, true, 0);
+	_wall2 = new Rigid_Body(true, true, 0);
+	_wall3 = new Rigid_Body(true, true, 0);
+	_wall4 = new Rigid_Body(true, true, 0);
+	_wall5 = new Rigid_Body(true, true, 0);
+	_wall6 = new Rigid_Body(true, true, 0);
+
+	_recObstacle1 = new Rigid_Body(true, true, 0);
+	_recObstacle2 = new Rigid_Body(true, true, 0);
+	_recObstacle3 = new Rigid_Body(true, true, 0);
+	_recObstacle4 = new Rigid_Body(true, true, 0);
+
+	_circObstacle1 = new Rigid_Body(true, true, 1);
+	_circObstacle2 = new Rigid_Body(true, true, 1);
+	_circObstacle3 = new Rigid_Body(true, true, 1);
+	_circObstacle4 = new Rigid_Body(true, true, 1);
+
+	_activeLVL = 0;
+	
+}
+
+Maps::~Maps()
+{
+}
+
+
+void Maps::addToVectorPool(std::vector<Rigid_Body*>& tempVec)
+{
+	tempVec.push_back(_wall1);
+	tempVec.push_back(_wall2);
+	tempVec.push_back(_wall3);
+	tempVec.push_back(_wall4);
+	tempVec.push_back(_wall5);
+	tempVec.push_back(_wall6);
+	tempVec.push_back(_recObstacle1);
+	tempVec.push_back(_recObstacle2);
+	tempVec.push_back(_recObstacle3);
+	tempVec.push_back(_recObstacle4);
+}
+
+void Maps::spawnMap1()
+{
+	_activeLVL = 1;
+
+	_wall1->SetPosition({ 400, 50 });
+	_wall2->SetPosition({ 125, 425 });
+	_wall3->SetPosition({ 380, 840 });
+	_wall4->SetPosition({ 675, 445 });
+
+	_wall1->SetSize({ 510, 40 });
+	_wall2->SetSize({ 40, 790 });
+	_wall3->SetSize({ 550, 40 });
+	_wall4->SetSize({ 40, 830 });
+
+	_wall1->SetColor({ 0, 0, 0, 255 });	  // black
+	_wall2->SetColor({ 255, 0, 0, 255 }); // red
+	_wall3->SetColor({ 0, 255, 0, 255 }); // green
+	_wall4->SetColor({ 0, 0, 255, 255 }); // blue
+
+
+	_wall1->SetRecOrigin();
+	_wall2->SetRecOrigin();
+	_wall3->SetRecOrigin();
+	_wall4->SetRecOrigin();
+}
+
+void Maps::spawnMap2()
+{
+	_activeLVL = 2;
+
+	_wall1->SetPosition({ 400, 800 });
+	_wall2->SetPosition({ 200, 580 });
+	_wall3->SetPosition({ 400, 120 });
+	_wall4->SetPosition({ 600, 580 });
+	_wall5->SetPosition({ 128, 260 });
+	_wall6->SetPosition({ 672, 260 }); 
+
+	_wall1->SetSize({ 440, 40 });
+	_wall2->SetSize({ 40, 400 });
+	_wall3->SetSize({ 730, 40 });
+	_wall4->SetSize({ 40, 400 });
+	_wall5->SetSize({ 300, 40 });
+	_wall6->SetSize({ 300, 40 });
+
+    _wall1->SetColor({ 0, 0, 255, 255 }); // blue
+    _wall2->SetColor({ 0, 0, 255, 255 }); // blue
+    _wall3->SetColor({ 0, 0, 255, 255 }); // blue
+    _wall4->SetColor({ 0, 0, 255, 255 }); // blue
+    _wall5->SetColor({ 0, 0, 255, 255 }); // blue
+    _wall6->SetColor({ 0, 0, 255, 255 }); // blue
+
+	_wall5->SetRotation(60);
+	_wall6->SetRotation(-60);
+
+
+	_recObstacle1->SetPosition({ 325, 350 });
+	_recObstacle2->SetPosition({ 325, 350 });
+
+	_recObstacle3->SetPosition({ 475, 350 });
+	_recObstacle4->SetPosition({ 475, 350 });
+
+	_recObstacle1->SetSize({ 100, 5 });
+	_recObstacle2->SetSize({ 100, 5 });
+	_recObstacle3->SetSize({ 100, 5 });
+	_recObstacle4->SetSize({ 100, 5 });
+
+	_recObstacle1->SetRotation(90);
+	_recObstacle3->SetRotation(90);
+
+	_wall1->SetRecOrigin();
+	_wall2->SetRecOrigin();
+	_wall3->SetRecOrigin();
+	_wall4->SetRecOrigin();
+	_wall5->SetRecOrigin();
+	_wall6->SetRecOrigin();
+	_recObstacle1->SetRecOrigin();
+	_recObstacle2->SetRecOrigin();
+	_recObstacle3->SetRecOrigin();
+	_recObstacle4->SetRecOrigin();
+}
+
+
+void Maps::Update()
+{
+	switch (_activeLVL)
+	{
+	case 1:
+		break;
+	case 2:
+		_recObstacle1->SpinRect(20);
+		_recObstacle2->SpinRect(20);
+		_recObstacle3->SpinRect(-20);
+		_recObstacle4->SpinRect(-20);
+		break;
+	}
+}
---
+++ Physics Engine Template/Maps.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include "Rigid_Body.h"
+
+class Maps
+{
+private:
+	// main walls
+	Rigid_Body* _wall1;
+	Rigid_Body* _wall2;
+	Rigid_Body* _wall3;
+	Rigid_Body* _wall4;
+	Rigid_Body* _wall5;
+	Rigid_Body* _wall6;
+
+	// map obstacles
+	Rigid_Body* _recObstacle1;
+	Rigid_Body* _recObstacle2;
+	Rigid_Body* _recObstacle3;
+	Rigid_Body* _recObstacle4;
+
+	Rigid_Body* _circObstacle1;
+	Rigid_Body* _circObstacle2;
+	Rigid_Body* _circObstacle3;
+	Rigid_Body* _circObstacle4;
+
+	std::vector<Rigid_Body*> _tempObjectTransferPool;
+	int _activeLVL;
+
+public:
+	void			addToVectorPool(std::vector<Rigid_Body*>& tempVec);
+	int 			getVectorPoolSize() { return _tempObjectTransferPool.size(); }
+
+	void			spawnMap1();
+	void			spawnMap2();
+
+	Maps();
+	~Maps();
+
+	void Update();
+};
+
