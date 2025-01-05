4ae27bb05d8891a0a4faf2cf5b2b6a83d640a68a
Raine <80836177+Your-Average-Arsonist@users.noreply.github.com>
2025-01-05 20:31:35 +0100

    INDENTSSSSSSSS

--- Physics Engine Template/Maps.h
+++ Physics Engine Template/Maps.h
@@ -7,35 +7,35 @@ class Maps
 {
 private:
 	// main walls
-	Rigid_Body* _wall1;
-	Rigid_Body* _wall2;
-	Rigid_Body* _wall3;
-	Rigid_Body* _wall4;
-	Rigid_Body* _wall5;
-	Rigid_Body* _wall6;
+	Rigid_Body*					_wall1;
+	Rigid_Body*					_wall2;
+	Rigid_Body*					_wall3;
+	Rigid_Body*					_wall4;
+	Rigid_Body*					_wall5;
+	Rigid_Body*					_wall6;
 
 	// map obstacles
-	Rigid_Body* _recObstacle1;
-	Rigid_Body* _recObstacle2;
-	Rigid_Body* _recObstacle3;
-	Rigid_Body* _recObstacle4;
+	Rigid_Body*					_recObstacle1;
+	Rigid_Body*					_recObstacle2;
+	Rigid_Body*					_recObstacle3;
+	Rigid_Body*					_recObstacle4;
 
-	Rigid_Body* _circObstacle1;
-	Rigid_Body* _circObstacle2;
-	Rigid_Body* _circObstacle3;
-	Rigid_Body* _circObstacle4;
+	Rigid_Body*					_circObstacle1;
+	Rigid_Body*					_circObstacle2;
+	Rigid_Body*					_circObstacle3;
+	Rigid_Body*					_circObstacle4;
 
 	Rigid_Body* _goal;
 
-	std::vector<Rigid_Body*> _tempObjectTransferPool;
+	std::vector<Rigid_Body*>	_tempObjectTransferPool;
 	int _activeLVL;
 
 public:
-	void			addToVectorPool(std::vector<Rigid_Body*>& tempVec);
-	int 			getVectorPoolSize() { return _tempObjectTransferPool.size(); }
+	void						addToVectorPool(std::vector<Rigid_Body*>& tempVec);
+	int 						getVectorPoolSize() { return _tempObjectTransferPool.size(); }
 
-	void			spawnMap1();
-	void			spawnMap2();
+	void						spawnMap1();
+	void						spawnMap2();
 
 	Maps();
 	~Maps();
